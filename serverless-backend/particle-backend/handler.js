9e45331d07390b3c8f61fc55477dbae9f800a208
Jan Sulaiman <jandaniel@outlook.de>
2020-03-28 17:33:54 +0100

    update handler to store event

--- serverless-backend/particle-backend/handler.js
+++ serverless-backend/particle-backend/handler.js
@@ -1,14 +1,61 @@
 'use strict';
 
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); 
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports.webook = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  const requestBody = JSON.parse(event.body);
+  const name = requestBody.name;
+  const data = requestBody.data;
+
+  if (typeof name !== 'string' || typeof data !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t submit candidate because of validation errors.'));
+    return;
+  }
+
+  submitEvent(events(name, data))
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Sucessfully stored event`,
+          candidateId: res.id
+        })
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to store event`
+        })
+      })
+    });
+
+};
+
+const submitEvent = event => {
+  console.log('Submitting event');
+  const events = {
+    TableName: process.env.EVENT_TABLE,
+    Item: event,
   };
+  return dynamoDb.put(events).promise()
+    .then(res => event);
+};
 
-  callback(null, response);
-
+const events = (name, data) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: uuid.v1(),
+    name: name,
+    data: data,
+    receivedAt: timestamp,
+  };
 };
